7efef7bcef291bb5c22ac26d7f95073204b80bc8
Maggie Podliian <mkanareika@gmail.com>
2021-04-15 19:20:29 +0200

    deprecated bpdyparser changed

--- twity-server/models/message.js
+++ twity-server/models/message.js
@@ -1,30 +1,36 @@
 const mongoose = require("mongoose");
 const User = require("./user");
 
-const messageSchema = new mongoose.Schema({
-  text: {
-    type: String,
-    required: true,
-    maxLength: 160,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User", 
-  },
-},  
+const messageSchema = new mongoose.Schema(
   {
-    timestamps: true,
-  });
+    text: {
+      type: String,
+      required: true,
+      maxLength: 160
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
 
 messageSchema.pre("remove", async function(next) {
   try {
+    // find a user
     let user = await User.findById(this.user);
-    user.message.remove(this.id);
+    // remove the id of the message from their messages list
+    user.messages.remove(this.id);
+    // save that user
     await user.save();
+    // return next
     return next();
-  } catch(err) {
+  } catch (err) {
     return next(err);
-  };s
+  }
 });
 
 const Message = mongoose.model("Message", messageSchema);
