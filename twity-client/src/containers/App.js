2db84a6cf8d01d9cd8b05c416aebf3b50812160a
Maggie Podliian <mkanareika@gmail.com>
2021-04-15 14:48:19 +0200

    Finished signin and signup

--- twity-client/src/containers/App.js
+++ twity-client/src/containers/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
-import { configureStore } from "../store"; 
+import { configureStore } from "../store";
 import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Navbar";
+import Main from "./Main";
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
     <Router>
-      <div>Hello World!</div>
+      <div className="onboarding">
+        <Navbar />
+        <Main />
+      </div>
     </Router>
   </Provider>
-)
+);
 
 export default App;
